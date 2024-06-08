7e58241f1871b44cb875b7a4e9ab811c9f16900b
scumenez <163432515+scumenez@users.noreply.github.com>
2024-06-08 16:15:08 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App.jsx";
 import "./index.css";
 import { TodoList2 } from "./Todo2.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
     <TodoList2 />
   </React.StrictMode>
 );
