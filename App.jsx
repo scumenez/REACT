1fd063c8d29f434e4d7e9f22ffe9fcb3cfba07f5
scumenez <163432515+scumenez@users.noreply.github.com>
2024-06-08 14:45:30 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useState , useEffect , useRef } from "react";
 import "./App.css";
 import CounterDisplay from "./CounterDisplay";
 
 export function App({ initialValue = 0, incrementBy = 1, decrementBy = 1 }) {
   const [counter, setCounter] = useState(initialValue);
+  const directionRef = useRef(null);
+  const previousCountRef = useRef(initialValue);
+
+  useEffect(() => {
+    if (counter > previousCountRef.current) {
+      directionRef.current = "up";
+    } else if (counter < previousCountRef.current) {
+      directionRef.current = "down";
+    }
+    if (directionRef.current !== previousCountRef.current) {
+      console.log(directionRef.current);
+    }
+    previousCountRef.current = counter;
+  }, [counter]);
 
   function handleIncrement() {
     setCounter((c) => c + incrementBy);
