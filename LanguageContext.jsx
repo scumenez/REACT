d3f0797d7ae8a30d7531bbf6738e2e54fd9230a7
scumenez <163432515+scumenez@users.noreply.github.com>
2024-06-09 15:53:15 +0200

    Delete LanguageContext.jsx

--- LanguageContext.jsx
+++
@@ -1,9 +0,0 @@
-/*import { createContext } from "react";
-
-export const languageContext = createContext("it");*/
-
-import React from "react";
-
-const LanguageContext = React.createContext();
-
-export default LanguageContext;
